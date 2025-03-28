48fbd5806e0d041ea11b173342d4df5f6779e7ef
Misha <mikhaillat@yandex.ru>
2025-03-28 14:05:50 +0300

    Initial commit

---
+++ src/main/java/ru/latypov/adapter/api/AdapterController.java
@@ -0,0 +1,4 @@
+package ru.latypov.adapter.api;
+
+public class AdapterController {
+}
---
+++ src/test/java/ru/latypov/adapter/AdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.latypov.adapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdapterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
